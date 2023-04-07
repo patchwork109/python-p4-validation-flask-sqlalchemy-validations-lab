7c189acd9f9acc6e947d4df56a5959cfcb5778fe
Samantha Genevay <samanthagenevay@gmail.com>
2023-04-07 09:57:34 -0700

    Completed Lab

--- server/debug.py
+++ server/debug.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
 
+from random import choice as rc
+
+from faker import Faker
+
 from app import app
 from models import db, Author, Post
 
 
+fake = Faker()
 
-if __name__ == '__main__':
-    
-    db.init_app(app)
-    with app.app_context():
-        import ipdb; ipdb.set_trace()
+with app.app_context():
+
+    Author.query.delete()
+    Post.query.delete()
+
+    # author1 = Author(name="John", phone_number= 1234)
+    # db.session.add(author1)
+    # db.session.commit()
+
+    post1 = Post(title="Hello", content="Lots of secrets" * 70, summary="Whole lotta secrets", category="Fiction")
+
+    # authors = []
+    # for n in range(25):
+    #     author = Author(name=fake.name(), phone_number='1324543333')
+    #     authors.append(author)
+
+    # db.session.add_all(authors)
+    # posts = []
+    # for n in range(25):
+    #     post = Post(title='Secret banana', content='This is the content Secret' * 50, category= 'Fiction', summary="Summary Secret" )
+    #     posts.append(post)
+
+    # db.session.add_all(posts)
+
+    # db.session.commit()
--- server/models.py
+++ server/models.py
@@ -3,7 +3,58 @@ from sqlalchemy.orm import validates
 db = SQLAlchemy()
 
 class Author(db.Model):
-    pass
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, unique=True, nullable=False)
+    phone_number = db.Column(db.Integer)
+
+    @validates('name')
+    def name_required(self, key, name):
+        names = Author.query.filter(Author.name).all()
+        if not name:
+            raise ValueError("Failed name validation. Name is required.")
+        elif name in names:
+            raise ValueError("Failed name validation. Name must be unique.")
+        return name
+    
+    @validates('phone_number')
+    def phone_number_validation(self, key, phone_number):
+        if len(str(phone_number)) != 10:
+            raise ValueError("Failed phone number validation. Number must be 10 digits.")
+        return phone_number
+
 
 class Post(db.Model):
-    pass
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    content = db.Column(db.String)
+    summary = db.Column(db.String)
+    category = db.Column(db.String)
+
+    @validates('title')
+    def title_validation(self, key, title):
+        if "Won't Believe" and "Secret" and "Top" and "Guess" not in title:
+            raise ValueError("Failed title validation. Title required and must be clickbaity.")
+        return title
+
+
+    @validates('content')
+    def content_validation(self, key, content):
+        if len(content) < 250:
+            raise ValueError("Failed content validation. Content must be greater than 250 words.")
+        return content
+    
+    @validates('summary')
+    def summary_validation(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError("Failed summary validation. Summary must be less than 250 words.")
+        return summary
+        
+    @validates('category')
+    def category_validation(self, key, category):
+        if category != "Fiction" and category != "Non-Fiction":
+            raise ValueError("Failed category validation. Category must be Fiction or Non-Fiction.")
+        return category
